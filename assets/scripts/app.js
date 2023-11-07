b9e79d4f5bc0bda725a8571b135543134b14b66e
Andrei Ciureanu <ConstantinAndrei.Ciureanu@endava.com>
2023-11-07 12:25:58 +0200

    refactored

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,22 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === "ATTACK") {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === "STRONG_ATTACK") {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    const playerDamage = dealPlayerDamage(maxDamage);
     currentPlayerHealth -= playerDamage;
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("you won");
@@ -19,5 +26,13 @@ function attackHandler() {
         alert("you have a draw");
     }
 }
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+}
 
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
